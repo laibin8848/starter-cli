a73227e1665ae205c69b0f930096d1fbf7d7dbd7
laibin8848 <laibin5@gdii-yueyun.com>
2021-07-19 10:40:37 +0800

    可自定义命令名称

--- src/init.js
+++ src/init.js
@@ -7,31 +7,27 @@ import symbol from 'log-symbols';
 const execFn = require('child_process').execSync;
 
 let init = async (projectName) => {
-    //项目不存在
     if (!fs.existsSync(projectName)) {
-        //命令行交互
         inquirer.prompt([
             {
                 name: 'description',
-                message: '请输入项目描述: '
+                message: 'Please enter project description: '
             },
             {
                 name: 'author',
-                message: '请输入作者: '
+                message: 'Please enter author: '
             },
             {
                 type: 'list',
                 name: 'install',
-                message: '是否自动安装依赖',
+                message: 'Auto do install',
                 choices: [
-                    { name: '是', value: 'yes' },
-                    { name: '否', value: 'no' }
+                    { name: 'yes', value: 'yes' },
+                    { name: 'no', value: 'no' }
                 ]
             }
         ]).then(async (answer) => {
-            //下载模板 选择模板
-            //通过配置文件，获取模板信息
-            let loading = ora('模板下载中 ...');
+            let loading = ora('downloading templates ...');
             loading.start();
             downloadLocal(projectName).then(() => {
                 loading.succeed();
@@ -42,23 +38,21 @@ let init = async (projectName) => {
                     json.name = projectName;
                     json.author = answer.author;
                     json.description = answer.description;
-                    //修改项目文件夹中 package.json 文件
                     fs.writeFileSync(fileName, JSON.stringify(json, null, '\t'), 'utf-8');
                     if(answer.install == 'yes') {
-                        loading = ora('安装依赖 ...');
+                        loading = ora('installing dependencies ...');
                         loading.start();
                         execFn(`cd ./${projectName} && npm install`);
                         loading.succeed();
                     }
-                    console.log(symbol.success, chalk.green('项目初始化完成!'));
+                    console.log(symbol.success, chalk.green('project initialization done!'));
                 }
             }, () => {
                 loading.fail();
             });
         });
-    }else {
-        //项目已经存在
-        console.log(symbol.error, chalk.red('项目已经存在'));
+    } else {
+        console.log(symbol.error, chalk.red('floder ['+projectName+'] already existed'));
     }
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
-import {version} from '../../package.json';
+import {version, command} from '../../package.json';
 
 export const VERSION = version;
 
+export const COMMAND = command;
+
 const HOME = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'];
 
-export const RC = `${HOME}/.stater-clirc`;
+export const RC = `${HOME}/.${COMMAND}rc`;
 
 export const DEFAULTS = {
     registry: 'vite-vue3-template',
