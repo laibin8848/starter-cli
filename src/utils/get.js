935f9ba6040662fd6457c62917d132744e6a31f1
laibin8848 <laibin5@gdii-yueyun.com>
2021-07-19 14:37:11 +0800

    更新说明文档

--- src/utils/get.js
+++ src/utils/get.js
@@ -1,10 +1,11 @@
 import { getAll } from './rc';
 import downloadGit from 'download-git-repo';
+import { DEFAULTS } from './constants';
 
 //下载模板
 export const downloadLocal = async (projectName) => {
     let config = await getAll();
-    let api = `github:${config.user}/${config.registry}`;
+    let api = `github:${config.user||DEFAULTS.user}/${config.registry||DEFAULTS.registry}`;
     return new Promise((resolve, reject) => {
         downloadGit(api, projectName, (err) => {
             if (err) {
